73eb49df893dd0612a1cf430595fd3f1d8afa0c6
Guillaume Boutin <guillaumeboutin2101@gmail.com>
2019-11-15 18:01:21 -0500

    Wrote migration for creating table .

---
+++ database/migrations/2019_11_15_224802_create_transactions.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('company_id');
+            $table->tinyInteger('payment_method_id');
+            $table->timestamps();
+
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->foreign('payment_method_id')->references('id')->on('payment_methods');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
