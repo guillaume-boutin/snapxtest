ccde4e0adf45ed2e8918ba3047464bbf8824cb2f
Guillaume Boutin <guillaumeboutin2101@gmail.com>
2019-11-15 17:47:17 -0500

    Migration for creating table .

--- database/migrations/2019_11_15_224226_create_table_payment_methods.php
+++ database/migrations/2019_11_15_224226_create_table_payment_methods.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableCompanies extends Migration
+class CreateTablePaymentMethods extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateTableCompanies extends Migration
      */
     public function up()
     {
-        Schema::create('companies', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
+        Schema::create('payment_methods', function (Blueprint $table) {
+            $table->tinyIncrements('id');
             $table->string('slug')->unique();
             $table->timestamps();
         });
@@ -28,6 +27,6 @@ class CreateTableCompanies extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('companies');
+        Schema::dropIfExists('payment_methods');
     }
 }
